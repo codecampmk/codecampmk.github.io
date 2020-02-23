1c165200c93a759f6212f6b097856fe856bca359
bojan.veljanovski <bojan.veljanovski@haselt.com>
2020-02-23 15:17:02 +0100

    first version of 2020 website

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace codecampmk
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebHost.CreateDefaultBuilder(args)
+                .UseWebRoot(".")
+                .Configure(x =>
+                {
+                    x.UseDefaultFiles();
+                    x.UseStaticFiles();
+                })
+                .Build()
+                .Run();
+        }
+    }
+}
